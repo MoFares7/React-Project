3fcc98c6a72d344034bcf1cfe7c5b3d18b6d800b
MoFares7 <faresdabbas1@gmail.com>
2022-10-10 22:22:08 +0300

    add css to sit

--- to-do-project/src/App.js
+++ to-do-project/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import TodoList from './components/TodoList/TodoList';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Heloo React</h1>
+      <TodoList />
     </div>
   );
 }
---
+++ to-do-project/src/components/TodoList/TodoList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './TodoList.css';
+
+
+
+class TodoList extends Component {
+
+    render() {
+        return (
+            <div className='todo-list container d-flex-column w-50 mt-5 pb-3'>
+            <h1>Welcome in out todo</h1>
+            </div>
+        );
+    }
+}
+export default TodoList;
